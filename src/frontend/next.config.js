505abdcd7b656634f7d34e077e8e50371ebe684c
abruneau <antonin.bruneau@datadoghq.com>
2025-03-05 16:52:14 +0100

    feat: add CORS headers configuration for API routes in Next.js

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -62,6 +62,24 @@ const nextConfig = {
     loader: 'custom',
     loaderFile: './utils/imageLoader.js',
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTION' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'traceparent, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
